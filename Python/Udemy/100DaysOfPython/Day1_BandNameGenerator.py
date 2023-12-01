eb0e3d887c3a4f1be35ae5b61f6653d250d27c62
kartik-sumathy <152595971+kartik-sumathy@users.noreply.github.com>
2023-12-01 08:52:27 +0000

    Python Day 1 Complete

---
+++ Python/Udemy/100DaysOfPython/Day1_BandNameGenerator.py
@@ -0,0 +1,10 @@
+
+
+print("Welcome to Band Name Generator")
+
+str_city_name = input("Enter Your City Name ")
+
+str_pet_name = input("Enter Your Pet Name ")
+
+print("Your Band Name is",str_city_name,str_pet_name) #used default separator here
+
